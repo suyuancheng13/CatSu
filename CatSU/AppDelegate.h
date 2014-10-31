7f085d13aad688df01c59ed90eba8ac133071bbf
SYC <suyuancheng13@163.com>
2014-10-31 09:31:11 +0800

    Initial Commit

---
+++ CatSU/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  CatSU
+//
+//  Created by Suyuancheng on 14-10-31.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
